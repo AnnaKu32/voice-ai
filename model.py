f215f4eae96a3adf73e595fedadb8ab51dd7f3ee
Ania <102367840+Ania123678@users.noreply.github.com>
2024-05-01 12:35:50 +0200

    Improve model accuracy

--- model.py
+++ model.py
@@ -9,11 +9,13 @@ from nltk.stem import WordNetLemmatizer, LancasterStemmer
 nltk.download('punkt')
 nltk.download('wordnet')
 
+from sklearn.model_selection import train_test_split
 import json 
 import numpy as np
 import random
 
-
+lemmatizer = WordNetLemmatizer()
+stemmer = LancasterStemmer()
 data = json.loads(open('intents.json').read())
 
 # ---------------------------------- Preprocessing ----------------------------------
@@ -34,10 +36,8 @@ for intent in data['intents']:
         classes.append(intent['tag'])
 
 
-lemmatizer = WordNetLemmatizer()
-stemmer = LancasterStemmer()
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+# words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
 
 words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
@@ -48,40 +48,54 @@ training = []
 output_empty = [0] * len(classes)
 
 for idx, doc in enumerate(data_x):
+    if not doc:
+        continue
     bag = []
+    pattern = doc[0]
     text = [lemmatizer.lemmatize(w.lower()) for w in doc]
-    text = [stemmer.stem(w.lower()) for w in doc]
+    # text = [lemmatizer.lemmatize(w.lower()) for w in pattern]
+    # text = [stemmer.stem(w.lower()) for w in doc]
     
     for w in words:
         bag.append(1) if w in text else bag.append(0)
         
     output_row = list(output_empty)
     output_row[classes.index(data_y[idx])] = 1
-    
     training.append([bag, output_row])
 
 random.shuffle(training)
 training = np.array(training, dtype=object)
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+#---------------------------------- Splitting Data ----------------------------------
+train_x=list(training[:,0])
+train_y=list(training[:,1]) 
+
+train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size=0.2)
 
 
 # ---------------------------------- Model ----------------------------------
 model = keras.Sequential()
-model.add(layers.Dense(256, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(len(train_y[0]), activation='softmax'))
 
 adam = optimizers.Adam(learning_rate=0.01, decay=1e-6)
-model.compile(loss='categorical_crossentropy', 
-              optimizer=adam, 
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+# sgd = optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+# model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 print(model.summary())
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+
+# ---------------------------------- Training ----------------------------------
+model.fit(np.array(train_x), np.array(train_y), epochs=150, verbose=1)
+loss, accuracy = model.evaluate(np.array(test_x), np.array(test_y), verbose=1)
+print(f"Test Loss: {loss}")
+print(f"Test Accuracy: {accuracy}")
+
+# hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+# model.save("chatbot_model.h5", hist)
+# print("model created")
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,7 @@ nltk.download('stopwords')
 
 SYMBOLS = '{}()[]\\.,:;+-_@*/#&$...…|<>=~^!?”“’"'
 STOPWORDS = set(stopwords.words('english'))
+ERROR_THRESHOLD = 0.25
 
 def clean_text(text):
     lemmatizer = WordNetLemmatizer()
@@ -21,30 +22,33 @@ def clean_text(text):
 def bag_of_words(text, vocab):
     tokens = clean_text(text)
     bag = [0] * len(vocab)
-    for idx, word in enumerate(vocab):
-        if word in tokens:
-            bag[idx] = 1
-    return bag
+    for w in tokens:
+        for idx, word in enumerate(vocab):
+            if word == w:
+                bag[idx] = 1
+    return np.array(bag)
 
 def pred_class(text, vocab, labels):
     bow = bag_of_words(text, vocab)
     res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append({'intent': labels[r[0]], 'probability': str(r[1])})
+        return_list.append(labels[r[0]])
     return return_list
 
 def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
-    return result
+    if len(intents_list) == 0:
+        return "I'm sorry, I don't understand."
+    else:
+        tag = intents_list[0]
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                result = random.choice(i['responses'])
+                break
+        return result
 
 
     
